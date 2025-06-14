b8c65019498f965e885f765be71af8ddfd343e3f
jaypawar12 <pawarjay684@gmail.com>
2025-06-14 18:17:13 +0530

    Working.....

--- Make Admin Panel On Requirements/routes/extraCategoryRoutes.js
+++ Make Admin Panel On Requirements/routes/extraCategoryRoutes.js
@@ -8,18 +8,18 @@ const {addExtraCategoryPage} = require('../controllers/extraCategoryCTR')
 
 // Add Category Page
 route.get('/addExtraCategoryPage', addExtraCategoryPage);
-route.post('/subCategoryInsert', upload.single('subCategory_image'), subCategoryInsert)
+// route.post('/subCategoryInsert', upload.single('subCategory_image'), subCategoryInsert)
 
-// View Category Page
-route.get('/viewSubCategoryPage', viewSubCategoryPage);
-
-
-// Edit Category
-route.get('/editSubCategoryPage/:id', editSubCategoryPage);
-route.post('/updateSubCategory/:id', upload.single('subCategory_image'), updateSubCategory);
+// // View Category Page
+// route.get('/viewSubCategoryPage', viewSubCategoryPage);
 
 
-// Delete Category
-route.get('/deleteSubCategory/:id', deleteSubCategory);
+// // Edit Category
+// route.get('/editSubCategoryPage/:id', editSubCategoryPage);
+// route.post('/updateSubCategory/:id', upload.single('subCategory_image'), updateSubCategory);
+
+
+// // Delete Category
+// route.get('/deleteSubCategory/:id', deleteSubCategory);
 
 module.exports = route;
